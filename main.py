e943701eff115e5fabf8d4d474a7c396b54f3fc0
Itsprakharyadav <prakharyadav.student3@gmail.com>
2022-09-18 17:45:03 +0000

    commit

--- main.py
+++ main.py
@@ -40,3 +40,47 @@ lass Login(Resource):
         else:
             return jsonify({"message": "Invalid email or password"})
 
+class Signup(Resource):
+
+    def post(self):
+        username = request.json['username']
+        email = request.json['email']
+        password = request.json['password']
+
+        # print(username, email, password)
+
+        email_exist_check = collection.find_one({'email': email})
+        username_exist_check = collection.find_one({'username': username})
+
+        if email_exist_check or username_exist_check:
+            return jsonify({"message": "Username or Email already exist"})
+        else:
+            collection.insert_one({
+                'username': username,
+                'email': email,
+                'password': password,
+                "appointments": []
+            })
+            return jsonify({"message": "User added succesfully"})
+
+class Signup(Resource):
+  pass
+
+class Schedule(Resource):
+  pass
+
+class MarkOff(Resource):
+  pass
+
+class ChangeNameAndPassword(Resource):
+  pass
+
+api.add_resource(Landing, '/')
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Schedule, '/schedule')
+api.add_resource(MarkOff, '/offhours')
+api.add_resource(ChangeNameAndPassword, '/change')
+
+if __name__ == '__main__':
+    app.run(debug=True)
